b69a470433a19bcd8fe223449d04ef6176020445
Josh Holly <joshholly@gmail.com>
2024-06-08 23:49:54 -0500

    Fixed filelions

--- src/providers/embeds/filelions.ts
+++ src/providers/embeds/filelions.ts
@@ -3,8 +3,6 @@ import { makeEmbed } from '@/providers/base';
 
 const linkRegex = /file: ?"(http.*?)"/;
 // the white space charecters may seem useless, but without them it breaks
-const tracksRegex = /\{file:\s"([^"]+)",\skind:\s"thumbnails"\}/g;
-
 export const filelionsScraper = makeEmbed({
   id: 'filelions',
   name: 'filelions',
@@ -16,11 +14,7 @@ export const filelionsScraper = makeEmbed({
       },
     });
     const mainPage = mainPageRes.body;
-    const mainPageUrl = new URL(mainPageRes.finalUrl);
-
     const streamUrl = mainPage.match(linkRegex) ?? [];
-    const thumbnailTrack = tracksRegex.exec(mainPage);
-
     const playlist = streamUrl[1];
     if (!playlist) throw new Error('Stream url not found');
 
@@ -32,14 +26,6 @@ export const filelionsScraper = makeEmbed({
           playlist,
           flags: [flags.IP_LOCKED, flags.CORS_ALLOWED],
           captions: [],
-          ...(thumbnailTrack
-            ? {
-                thumbnailTrack: {
-                  type: 'vtt',
-                  url: mainPageUrl.origin + thumbnailTrack[1],
-                },
-              }
-            : {}),
         },
       ],
     };
