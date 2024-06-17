ac87e0daa6e1cdc20748ca591cc87870802a73cf
Josh Holly <joshholly@gmail.com>
2024-06-17 03:09:17 -0500

    Added progress progession to nova scraper

--- src/providers/embeds/nova.ts
+++ src/providers/embeds/nova.ts
@@ -1,5 +1,6 @@
 import { EmbedOutput, makeEmbed } from '@/providers/base';
 import { baseUrl } from '@/providers/sources/whvx';
+import { NotFoundError } from '@/utils/errors';
 
 export const novaScraper = makeEmbed({
   id: 'nova',
@@ -7,10 +8,30 @@ export const novaScraper = makeEmbed({
   rank: 670,
   disabled: false,
   async scrape(ctx) {
-    const search = await ctx.fetcher(`${baseUrl}/search?query=${encodeURIComponent(ctx.url)}`);
-    ctx.progress(50);
-    const result = await ctx.fetcher(`${baseUrl}/source?resourceId=${encodeURIComponent(search.url)}`);
-    ctx.progress(100);
-    return result as EmbedOutput;
+    let progress = 50;
+    const interval = setInterval(() => {
+      if (progress < 100) {
+        progress += 1;
+        ctx.progress(progress);
+      }
+    }, 100);
+
+    try {
+      const search = await ctx.fetcher(`${baseUrl}/search?query=${encodeURIComponent(ctx.url)}`);
+      if (search.statusCode === 404) {
+        throw new Error('No files found');
+      }
+      const result = await ctx.fetcher(`${baseUrl}/source?resourceId=${encodeURIComponent(search.url)}`);
+      if (result.statusCode === 404) {
+        throw new Error('No streams found');
+      }
+      clearInterval(interval);
+      ctx.progress(100);
+      return result as EmbedOutput;
+    } catch (error) {
+      clearInterval(interval);
+      ctx.progress(100);
+      throw new NotFoundError('Failed to search');
+    }
   },
 });
